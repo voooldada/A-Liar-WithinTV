da0476922bc415e60863b7eddb9a7dd2b0f6dc2f
voooldada <vooolcanicalgames@gmail.com>
2026-05-26 18:49:04 +0100

    Delete tv.js

--- tv.js
+++
@@ -1,3 +0,0 @@
-// tv.js - Script vazio pois tv.html agora gera QR code estático com inline JS
-// Este arquivo mantém compatibilidade com estrutura modular se necessário
-console.log('📺 TV Broadcast carregado');
